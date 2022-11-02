a1bebde306ecff20d8f5d6037539262cb332d2d9
pitiK3U <djhlinacz@gmail.com>
2022-11-02 16:37:55 +0100

    stm32cxx: Implement method for getting timer's counter

--- softwareComponents/stm32cxx/src/drivers/timer.hpp
+++ softwareComponents/stm32cxx/src/drivers/timer.hpp
@@ -31,6 +31,10 @@ public:
         LL_TIM_DisableCounter( _periph );
     }
 
+    uint32_t counter() {
+        return LL_TIM_GetCounter( _periph );
+    }
+
     class Pwm {
     public:
         Pwm(): _periph( nullptr ), _channel( 0 ) {};
