384c38ce68cc94ac0112c829893a91cdb224e4af
pitiK3U <djhlinacz@gmail.com>
2023-02-14 16:53:48 +0100

    roficom: Simplify lidar usage

--- hardwareModules/RoFICoM/software/i2c_test/src/main.cpp
+++ hardwareModules/RoFICoM/software/i2c_test/src/main.cpp
@@ -1,14 +1,14 @@
-#include <system/defer.hpp>
-
 #include <atoms/result.hpp>
 // #include <atoms/util.hpp>
 
 #include <cassert>
+
+#include <system/defer.hpp>
 #include <system/dbg.hpp>
+
 #include <drivers/clock.hpp>
 #include <drivers/gpio.hpp>
 #include <drivers/timer.hpp>
-
 #include <drivers/i2c.hpp>
 
 #include <lidar.hpp>
@@ -20,10 +20,6 @@
 #include <stm32g0xx_ll_utils.h>
 
 #include <stm32g0xx_ll_i2c.h>
-
-#include <vl53l1_api.h>
-#include <vl53l1_platform.h>
-#include <vl53l1_platform_init.h>
 
 
 void setupSystemClock() {
@@ -56,53 +52,6 @@ void setupSystemClock() {
     LL_RCC_SetUSARTClockSource( LL_RCC_USART2_CLKSOURCE_PCLK1 );
 }
 
-void setupI2C() {
-    LL_GPIO_InitTypeDef GPIO_InitStruct = { };
-
-    LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
-    /**I2C2 GPIO Configuration
-    PA11 [PA9]   ------> I2C2_SCL
-    PA12 [PA10]   ------> I2C2_SDA
-    */
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_11;
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-    GPIO_InitStruct.Alternate = LL_GPIO_AF_6;
-    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_12;
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-    GPIO_InitStruct.Alternate = LL_GPIO_AF_6;
-    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    /* Peripheral clock enable */
-    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C2);
-
-    LL_I2C_InitTypeDef I2C_InitStruct = { };
-
-    I2C_InitStruct.PeripheralMode = LL_I2C_MODE_I2C;
-    // This parameter is computed by STM32cubemx tool
-    I2C_InitStruct.Timing = 0x00303D5B;
-    I2C_InitStruct.AnalogFilter = LL_I2C_ANALOGFILTER_ENABLE;
-    I2C_InitStruct.DigitalFilter = 0;
-    I2C_InitStruct.OwnAddress1 = 0;
-    I2C_InitStruct.TypeAcknowledge = LL_I2C_ACK;
-    I2C_InitStruct.OwnAddrSize = LL_I2C_OWNADDRESS1_7BIT;
-
-    LL_I2C_Init(I2C2, &I2C_InitStruct);
-    LL_I2C_EnableAutoEndMode(I2C2);
-    LL_I2C_SetOwnAddress2(I2C2, 0, LL_I2C_OWNADDRESS2_NOMASK);
-    LL_I2C_DisableOwnAddress2(I2C2);
-    LL_I2C_DisableGeneralCall(I2C2);
-    LL_I2C_EnableClockStretching(I2C2);
-}
-
-
 using Block = memory::Pool::Block;
 
 Dbg& dbgInstance() {
@@ -114,56 +63,6 @@ Dbg& dbgInstance() {
     return inst;
 }
 
-// Helper class for printing error on `Result<Void, std::string>`
-class PrintError {
-    public:
-        void operator()( std::true_type, auto ) { }
-        void operator()( std::false_type, const std::string& error ) { Dbg::error( error.c_str() ); }
-};
-
-void lidarMeasure() {
-    Lidar lidar( I2C( I2C2, SdaPin( GpioA[ 12 ] ), SclPin( GpioA[ 11 ] ) ) );
-
-    Dbg::info("VL53L1 init start\n");
-    PrintError printError;
-    lidar.initialize().match( printError );
-
-    Dbg::info("start measuring\n");
-
-    lidar.startMeasurement().match( printError );
-
-    while ( true ) {
-        auto ready = lidar.waitMeasurementDataReady();
-        if ( !ready ) {
-            ready.match( printError );
-            break;
-        }
-
-        auto rangingMeasurementData = lidar.getRangingMeasurementData();
-        if ( !rangingMeasurementData ) {
-            rangingMeasurementData.match( printError );
-            break;
-        }
-
-        Dbg::info("Range status: %d, Range: %d mm\n",
-            rangingMeasurementData->RangeStatus,
-            rangingMeasurementData->RangeMilliMeter);
-
-        ready = lidar.clearInterruptAndStartMeasurement();
-        if ( !ready ) {
-            ready.match( printError );
-            break;
-        }
-    }
-
-    auto ready = lidar.stopMeasurement();
-    if ( !ready ) {
-        ready.match( printError );
-    }
-
-    Dbg::info( "End measure()" );
-}
-
 int main() {
     setupSystemClock();
     SystemCoreClockUpdate();
@@ -171,13 +70,42 @@ int main() {
 
     Dbg::info( "Main clock: %d", SystemCoreClock );
 
-    // LL_mDelay(200);
+    Lidar lidar( I2C( I2C2, SdaPin( GpioA[ 12 ] ), SclPin( GpioA[ 11 ] ) ) );
 
-    lidarMeasure();
+    Dbg::info("VL53L1 init start\n");
+
+    auto result = lidar.initialize().and_then( [&] ( auto ) {
+        Dbg::info("start measuring\n");
+        return lidar.startMeasurement();
+    });
+
+    if ( !result ) {
+        Dbg::error( "\n" );
+        Dbg::error( result.assume_error().data() );
+        return 1;
+    }
 
     while ( true ) {
         // uint16_t sensor_id;
         // VL53L1X_ULP_GetSensorId(0x52, &sensor_id);
+
+        result = lidar.getMeasurementDataReady().and_then( [&] ( bool ready ) {
+            return !ready
+            ? atoms::make_result_value< atoms::Void >()
+            : lidar.getRangingMeasurementData()
+                .and_then( [&] ( auto rangingMeasurementData ) {
+                    Dbg::info( "Range status: %d, Range: %d mm\n",
+                        rangingMeasurementData.RangeStatus,
+                        rangingMeasurementData.RangeMilliMeter );
+
+                    return lidar.clearInterruptAndStartMeasurement();
+                });
+        });
+
+        if ( !result ) {
+            Dbg::error( result.assume_error().data() );
+            return 2;
+        }
 
         if ( Dbg::available() ) {
             switch( Dbg::get() ) {
@@ -185,9 +113,13 @@ int main() {
                 Dbg::error( "DBG received: %c", Dbg::get() );
             }
         }
-        if (Defer::run()) {
+        if ( Defer::run() ) {
             // Dbg::error("D\n");
         }
     }
+
+    Dbg::info( "End measure()" );
+
+    lidar.stopMeasurement();
 }
 
